773232daaff2316e04cf83f6123bdfe9d31d4e52
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2014-03-13 00:30:25 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= '2i7HcWIzMJJlBdDGfMaYfBr57GBJimiSzBQ1TClkBUgTOQnq'
+		defaultFeeds	= [1034807551], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Sleep Tracking with ArduPillow', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
